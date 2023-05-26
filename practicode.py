01dc362043f730e0e6cbe1e2ac9c44fb42f77653
Mariano Andolina <mariano.3gs@gmail.com>
2023-05-26 09:12:57 -0300

    3er commit

--- practicode.py
+++ practicode.py
@@ -28,4 +28,29 @@
 # else:
 #   print("i is no longer less than 6")
 
+#Loop through the items in the fruits list.
+
+# fruits = ["apple", "banana", "cherry"]
+
+# for x in fruits:
+#     if x == "banana":
+#         continue
+#     print(x)
+
+"""
+Nested Loops
+A nested loop is a loop inside a loop.
+
+The "inner loop" will be executed one time for each iteration of the "outer loop":
+"""
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+    for y in fruits:
+        print(x,y)
+
         
+ 
+
+
