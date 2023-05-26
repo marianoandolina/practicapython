d91fa674d486e622598638d7c9ffdab8ef71ca65
Mariano Andolina <mariano.3gs@gmail.com>
2023-05-26 08:55:04 -0300

    2 commit viernes practicode

---
+++ practicode.py
@@ -0,0 +1,31 @@
+#Stop the loop if i is 3
+
+# i = 0
+
+# while i < 5:
+#     print(f"i vale {i}")
+#     i = i + 1
+#     if i == 3:
+#         break
+
+#In the loop, when i is 3, jump directly to the next iteration.
+
+# i = 0
+
+# while i < 3:
+#     print("i todavia vale menos que 3")
+#     i += 1
+#     if i == 3:
+#         continue
+#     print("i vale 3")
+
+#Print a message once the condition is false.
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+# else:
+#   print("i is no longer less than 6")
+
+        
